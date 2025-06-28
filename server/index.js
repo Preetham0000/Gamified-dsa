ff990977d2222cb537bf129f242333b370ec1444
PREETHAM H <136721305+Preetham0000@users.noreply.github.com>
2025-06-28 16:00:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
 
 // Serve built frontend
 app.use(express.static(path.join(__dirname, '../public')));
