d9f4994e221b3ab575ae511b31f1b49d1f67befc
PREETHAM H <136721305+Preetham0000@users.noreply.github.com>
2025-07-02 15:58:16 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,8 +4,7 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// // router.post('/login', authUser);
-// router.post('/register', registerUser);
+ 
 router.get('/:userId/progress', protect, getUserProgress);
 
 export default router;
